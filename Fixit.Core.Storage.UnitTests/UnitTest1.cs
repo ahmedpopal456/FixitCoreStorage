39d3513f43e8f9e357753c43fee344e6d4f996b4
Joanna Lin <33985435+jayohanna@users.noreply.github.com>
2020-12-28 16:48:09 -0500

    (#388) Created projects

---
+++ Fixit.Core.Storage.UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fixit.Core.Storage.UnitTests
+{
+  [TestClass]
+  public class UnitTest1
+  {
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+  }
+}
---
+++ Fixit.Core.Storage/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Fixit.Core.Storage
+{
+  public class Class1
+  {
+  }
+}
